f0fb36a451ddefd4b6e0d75a62fed003c0633be9
CroyQian <1751701545@qq.com>
2022-04-27 11:05:18 +0800

    leetcode每日一题：太平洋大西洋水流问题 bfs解法

---
+++ algorithm/src/main/java/bfs/PacificAtlanticWaterFlow.java
@@ -0,0 +1,76 @@
+package bfs;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 太平洋大西洋水流问题
+ *
+ * @author Croy Qian
+ * @createDate 2022/4/27
+ * @Description 有一个 m × n 的矩形岛屿，与 太平洋 和 大西洋 相邻。 “太平洋” 处于大陆的左边界和上边界，而 “大西洋” 处于大陆的右边界和下边界。
+ * 这个岛被分割成一个由若干方形单元格组成的网格。给定一个 m x n 的整数矩阵heights，heights[r][c]表示坐标 (r, c) 上单元格 高于海平面的高度 。
+ * 岛上雨水较多，如果相邻单元格的高度 小于或等于 当前单元格的高度，雨水可以直接向北、南、东、西流向相邻单元格。水可以从海洋附近的任何单元格流入海洋。
+ * 返回 网格坐标 result 的 2D列表 ，其中 result[i] = [ri, ci] 表示雨水可以从单元格 (ri, ci) 流向 太平洋和大西洋 。
+ * @see <a href="https://leetcode-cn.com/problems/pacific-atlantic-water-flow/"></a>
+ */
+public class PacificAtlanticWaterFlow {
+    static int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+    int[][] heights;
+    int m, n;
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        this.heights = heights;
+        this.m = heights.length;
+        this.n = heights[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            bfs(i, 0, pacific);
+        }
+        for (int j = 1; j < n; j++) {
+            bfs(0, j, pacific);
+        }
+        for (int i = 0; i < m; i++) {
+            bfs(i, n - 1, atlantic);
+        }
+        for (int j = 0; j < n - 1; j++) {
+            bfs(m - 1, j, atlantic);
+        }
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    List<Integer> cell = new ArrayList<Integer>();
+                    cell.add(i);
+                    cell.add(j);
+                    result.add(cell);
+                }
+            }
+        }
+        return result;
+    }
+
+    public void bfs(int row, int col, boolean[][] ocean) {
+        if (ocean[row][col]) {
+            return;
+        }
+        ocean[row][col] = true;
+        Queue<int[]> queue = new ArrayDeque<int[]>();
+        queue.offer(new int[] { row, col });
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            for (int[] dir : dirs) {
+                int newRow = cell[0] + dir[0], newCol = cell[1] + dir[1];
+                if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n
+                        && heights[newRow][newCol] >= heights[cell[0]][cell[1]] && !ocean[newRow][newCol]) {
+                    ocean[newRow][newCol] = true;
+                    queue.offer(new int[] { newRow, newCol });
+                }
+            }
+        }
+    }
+
+}
