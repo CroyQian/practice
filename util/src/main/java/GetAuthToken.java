a6ce9c9ce61de2cf31e08deda1a539666deac2b0
wh_qianp <v-qianp.gd@chinatelecom.cn>
2025-05-08 14:14:26 +0800

    工具类

--- util/src/main/java/GetAuthToken.java
+++ util/src/main/java/GetAuthToken.java
@@ -20,11 +20,11 @@ public class GetAuthToken {
      * @param echoStr
      * @return
      */
-    private static String getSignature(String date, String appid, String appKey, String echoStr) {
+    private static String getSignature(String date, String appid, String appKey, String echoStr, String mdType) {
         MessageDigest md;
         StringBuffer sb = new StringBuffer();
         try {
-            md = MessageDigest.getInstance("SHA-1");
+            md = MessageDigest.getInstance(mdType);
             md.update(date.getBytes("UTF-8"));
             md.update(appid.getBytes("UTF-8"));
             md.update(appKey.getBytes("UTF-8"));
@@ -48,17 +48,33 @@ public class GetAuthToken {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入appId(测试环境默认CBS)");
+        String appId = scanner.nextLine();
+        System.out.println("请输入appKey(测试环境默认123456Ab)");
+        String appKey = scanner.nextLine();
+        System.out.println("请选择生成方式(1:SHA-1,2:SHA-256)");
+        String mdType = "";
+        while (true) {
+            String type = scanner.nextLine();
+            if (type.equals("1")) {
+                mdType = "SHA-1";
+                break;
+            } else if (type.equals("2")) {
+                mdType = "SHA-256";
+                break;
+            } else {
+                System.out.println("请选择生成方式(1:SHA-1,2:SHA-256)");
+            }
+        }
         System.out.println("请输入g生成token");
         while (true) {
-            Scanner scanner = new Scanner(System.in);
             if (scanner.nextLine().equals("g")) {
                 String echoStr = UUID.randomUUID().toString();
                 System.out.println("uuid=" + echoStr);
                 String date = new SimpleDateFormat("yyyyMMddHHmm").format(new Date());
                 System.out.println("date=" + date);
-                System.out.println(
-                        "authToken:" + "@" + getSignature(date, "CBS", "123456Ab", echoStr) + "|" + date + "|" + "CBS" + "|"
-                                + echoStr);
+                System.out.println("authToken:" + "@" + getSignature(date, appId, appKey, echoStr, mdType) + "|" + date + "|" + appId + "|" + echoStr);
             } else {
                 System.out.println("请重新输入");
             }
--- util/src/main/java/ReplaceIpHost.java
+++ util/src/main/java/ReplaceIpHost.java
@@ -1,6 +1,108 @@
-package PACKAGE_NAME;/**
- *
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
  * @author Croy Qian
  */
 public class ReplaceIpHost {
+    public static String replaceHost(String url){
+        String relUrl="";
+        if(StringUtils.isEmpty(url)){
+            return url;
+        }
+
+        String host=getHost(url);
+        if(StringUtils.isEmpty(host)){
+            System.out.println("提取host:" + host+",返回url:"+url);
+            return url;
+        }
+
+        String proxyUrl="www.baidu.com";
+
+        if(StringUtils.isEmpty(proxyUrl)){
+            System.out.println("未获取到需要替换的url:" + host);
+            return url;
+        }
+        relUrl= url.replace(host, proxyUrl);
+
+        //        if(!"https".equals(relUrl.substring(0,5).toLowerCase())){
+        //            relUrl="https"+relUrl.substring(4);//http替换成https
+        //        }
+
+        return relUrl;
+    }
+
+    private static String getHost(String url){
+        if(StringUtils.isEmpty(url)){
+            return url;
+        }
+
+        String host = "";
+        // "(?<=//|)((\\\\w)+\\\\.)+\\\\w+(\\\\:(\\\\w)+)*"
+        Pattern p =  Pattern.compile("^(http://|https://)((\\w)+\\.)+\\w+(\\:(\\w)+)*");
+        Matcher matcher = p.matcher(url);
+        if(matcher.find()){
+            host = matcher.group();
+        }
+        return host;
+    }
+
+    public static void main(String[] args) {
+        // 原始URL
+        String originalUrl = "http://10.34.21.210:19300/statics/2024/07/29/24112000000094149929_b0eb1a10f1594d6db49a67694332b236.pdf";
+
+        // 使用URL类解析原始URL
+        try {
+            java.net.URL url = new java.net.URL(originalUrl);
+
+            // 获取协议和主机名
+            String protocol = url.getProtocol();
+            String host = url.getHost();
+
+            // 新的主机名
+            String newHost = "10.34.21.210:22";
+
+            // 构建新的URL字符串
+            String newUrl = protocol + "://" + newHost + url.getPath();
+
+            // 如果URL中有查询字符串，也需要添加上
+            if (url.getQuery() != null) {
+                newUrl += "?" + url.getQuery();
+            }
+
+            System.out.println("原始URL: " + originalUrl);
+            System.out.println("修改后的URL: " + newUrl);
+
+        } catch (java.net.MalformedURLException e) {
+            System.err.println("无效的URL格式");
+        }
+    }
+
+    private String replaceAddrToNewHost(String originalUrl) {
+        try {
+            java.net.URL url = new java.net.URL(originalUrl);
+            // 获取协议和主机名
+            String protocol = url.getProtocol();
+            String host = url.getHost();
+            // 新的主机名
+            String newHost = "www.baidu.com";
+            if (StringUtils.isNotBlank(newHost)) {
+                // 构建新的URL字符串
+                String newUrl = protocol + "://" + newHost + url.getPath();
+                // 如果URL中有查询字符串，也需要添加上
+                if (url.getQuery() != null) {
+                    newUrl += "?" + url.getQuery();
+                }
+//                getLogger().info("邮件推送进行url替换,原始URL:" + originalUrl + ",替换后的URL:" + newUrl);
+                return newUrl;
+            } else {
+                return originalUrl;
+            }
+        } catch (java.net.MalformedURLException e) {
+//            getLogger().error(e.getMessage());
+            return originalUrl;
+        }
+    }
 }
